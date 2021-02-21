b1353937d4f68ab7eb7f6b91a99072f72ee9803d
Shubham Sharma <shubhash@microsoft.com>
2021-02-21 12:35:56 +0530

    Add concurrent update handling
    Signed-off-by: Shubham Sharma <shubhash@microsoft.com>

--- pkg/store/store.go
+++ pkg/store/store.go
@@ -2,25 +2,35 @@ package store
 
 import (
 	"errors"
+	"sync"
 )
 
 // ErrorKeyNotFound is returned by the Get method to indicate
 // that the key was not present in the store.
 var ErrorKeyNotFound = errors.New("Key not found")
 
-var store = make(map[string]string)
+var store = struct {
+	sync.RWMutex
+	m map[string]string
+}{m: make(map[string]string)}
 
 // Put a value in the store against a key. If the key already exists,
 // it is overwritten.
 func Put(k string, v string) error {
-	store[k] = v
+	store.Lock()
+	store.m[k] = v
+	store.Unlock()
+
 	return nil
 }
 
 // Get returns a value from the store associated with a key.
 // Returns ErrorKeyNotFound if key does not exist.
 func Get(k string) (string, error) {
-	v, ok := store[k]
+	store.RLock()
+	v, ok := store.m[k]
+	store.RUnlock()
+
 	if !ok {
 		return "", ErrorKeyNotFound
 	}
@@ -31,6 +41,6 @@ func Get(k string) (string, error) {
 // Delete ensures that a key does not exist in the store.
 // If a key is missing, the function passes silently.
 func Delete(k string) error {
-	delete(store, k)
+	delete(store.m, k)
 	return nil
 }
